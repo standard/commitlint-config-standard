1af7a8a6ef2ec67006b655c2c92c94a879133cfd
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-08-27 13:52:29 +0700

    specify type-enum

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  'type-enum': ['feat', 'fix', 'style', 'refactor', 'test', 'revert']
 }
